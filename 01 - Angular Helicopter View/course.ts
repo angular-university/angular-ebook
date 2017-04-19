9bdbdc23b7541914df9f7794e56ef4cd37e9a9df
JHades <jhades.dev@gmail.com>
2017-04-19 11:19:18 +0200

    Angular ebook support code

---
+++ 01 - Angular Helicopter View/course.ts
@@ -0,0 +1,19 @@
+
+
+export const course =
+{
+  "summary": {
+    "id": 9,
+    "url": "angular2-http",
+    "description": "Angular RxJs Jumpstart",
+    "longDescription": "Learn RxJs quickly, apply it to learn how to use the HTTP module effectively",
+    "totalLessons": 15,
+    "comingSoon": false,
+    "isNew": false,
+    "isOngoing": false,
+    "visibleFrom": "1970-01-31T23:00:00.000Z",
+    "iconUrl": "https://angular-academy.s3.amazonaws.com/thumbnails/angular-http-v2.png",
+    "courseListIcon": "https://angular-academy.s3.amazonaws.com/course-logos/observables_rxjs.png"
+  },
+  "lessons": []
+};
