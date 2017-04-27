c50fa386f0e207320d7e52f1dced7a987c552ff3
JHades <jhades.dev@gmail.com>
2017-04-27 16:18:28 +0200

    angular vs jQuery chapter

--- 02-03 - Angular vs jQuery/hello-world-jquery/index.js
+++ 02-03 - Angular vs jQuery/hello-world-jquery/index.js
@@ -19,12 +19,41 @@
                 lessons[index].description = index + ' - ' + lessons[index].description
             });
 
-            $("#lessons").html(generateHtml(lessons));
+           var lessonsDiv = document.getElementById('lessons');
+
+           // delete all child elements
+           while (lessonsDiv.firstChild) {
+               lessonsDiv.removeChild(lessonsDiv.firstChild);
+           }
+
+           // apply the table directly to the DOM
+           lessonsDiv.appendChild(generateDomTable(lessons));
 
         });
     });
 
 }());
+
+function generateDomTable(lessons) {
+
+    var table = document.createElement('table');
+
+    var header = table.createTHead();
+    var row = header.insertRow(0);
+    var cell = row.insertCell(0);
+    cell.innerHTML = "Description";
+
+    for (var i = 0; i < lessons.length; i++) {
+
+        var row = table.insertRow(i);
+        var col = row.insertCell(0);
+
+        col.innerHTML = lessons[i].description;
+    }
+
+    return table;
+}
+
 
 function generateHtml(lessons) {
     var html = "<table class='table lessons-list'>" +
@@ -43,4 +72,5 @@ function generateHtml(lessons) {
 
     return html;
 }
+
 
--- 02-03 - Angular vs jQuery/ng-hello-world/src/app/app.component.ts
+++ 02-03 - Angular vs jQuery/ng-hello-world/src/app/app.component.ts
@@ -9,12 +9,19 @@ export class AppComponent implements OnInit {
 
   lessons: any[];
 
-  constructor(private http:Http) {}
+  constructor(private http: Http) {
+  }
 
   ngOnInit() {
     this.http.get('https://final-project-recording.firebaseio.com/lessons.json')
       .map(res => Object.values(res.json()))
       .subscribe(lessons => this.lessons = lessons);
+  }
+
+  updateCourses() {
+    this.lessons.forEach((lesson, index) => {
+      this.lessons[index].description = index + ' - ' + this.lessons[index].description
+    });
   }
 
 }
