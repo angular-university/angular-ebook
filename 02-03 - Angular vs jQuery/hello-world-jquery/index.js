928db24ffa17494f2266531e752401b4069dd91a
JHades <jhades.dev@gmail.com>
2017-04-27 15:30:35 +0200

    angular vs jQuery chapter

--- 02-03 - Angular vs jQuery/hello-world-jquery/index.js
+++ 02-03 - Angular vs jQuery/hello-world-jquery/index.js
@@ -1,25 +1,46 @@
 
+(function(){
 
+    var lessons;
 
-$.get( "https://final-project-recording.firebaseio.com/lessons.json", function( data ) {
+    $(document).ready(function() {
 
-    var lessons = Object.values(data);
+        $.get( "https://final-project-recording.firebaseio.com/lessons.json", function( data ) {
 
-    var html = "<table class='table lessons-list'>' + " +
-                   "'<thead>" +
-                        "<th>Description</th>" +
-                    "</thead>" +
-                    "<tbody>";
+            lessons = Object.values(data);
+
+            $("#lessons").html(generateHtml(lessons));
+
+        });
+
+       $('#btn').on('click',function() {
+
+            lessons.forEach(function(lesson, index) {
+                lessons[index].description = index + ' - ' + lessons[index].description
+            });
+
+            $("#lessons").html(generateHtml(lessons));
+
+        });
+    });
+
+}());
+
+function generateHtml(lessons) {
+    var html = "<table class='table lessons-list'>" +
+        "<thead>" +
+        "<th>Description</th>" +
+        "</thead>" +
+        "<tbody>";
 
     lessons.forEach(function(lesson) {
-        html += '<tr>' +  +
-                    '<td>' + lesson.description + '</td>' +
-                '</tr>';
+        html += '<tr>' +
+                '<td>' + lesson.description + '</td>' +
+            '</tr>';
     });
 
     html += '</tbody></table>';
 
-    $("#lessons").html(html);
+    return html;
+}
 
-
-});
